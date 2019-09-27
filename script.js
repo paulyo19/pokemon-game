a4f46357bbf47f0f43077092dec5ee6260bb6c02
paulyo19 <catkillercleary@gmail.com>
2019-09-27 23:02:06 +0100

    both pokemon health bars go down after attacks

--- script.js
+++ script.js
@@ -70,7 +70,9 @@ var gameState = {
 
         // current user and cpu pokemon initail health
         gameState.currentPokemon[0].health = gameState. calculateInitialHealth(gameState.currentPokemon)
+        gameState.currentPokemon[0].originalHealth = gameState. calculateInitialHealth(gameState.currentPokemon)
         gameState.currentRivalPokemon[0].health = gameState. calculateInitialHealth(gameState.currentRivalPokemon)
+        gameState.currentRivalPokemon[0].originalHealth = gameState. calculateInitialHealth(gameState.currentRivalPokemon)
         console.log(gameState)
       }
       i++
@@ -102,6 +104,18 @@ var gameState = {
     console.log(enemy.name + ' before: ' + enemy.health)
     var attackAmount = ((attack * level) * (stack + critical))
     enemy.health = enemy.health - attackAmount
+    
+    var userHP = document.querySelector('.player1').querySelector('.stats').querySelector('.health').querySelector('.health-bar').querySelector('.inside')
+    var cpuHP = document.querySelector('.player2').querySelector('.stats').querySelector('.health').querySelector('.health-bar').querySelector('.inside')
+
+    if (enemy.owner == 'user') {
+     var minusPercent = ((enemy.health * 100) / enemy.originalHealth)
+     userHP.style.width = ((minusPercent < 0) ? 0 : minusPercent) + '%'
+    } else {
+      var minusPercent = ((enemy.health * 100) / enemy.originalHealth)
+     cpuHP.style.width = ((minusPercent < 0) ? 0 : minusPercent) + '%'
+    }
+
     gameState. checkWinner(enemy, attacker)
     console.log(enemy.name + ' after: ' + enemy.health)
   },
@@ -123,6 +137,8 @@ var gameState = {
   play: function (userAttack, cpuAttack) {
     var currentPokemon = gameState.currentPokemon[0]
     var currentRivalPokemon = gameState.currentRivalPokemon[0]
+    currentPokemon.owner = 'user'
+    currentRivalPokemon.owner = 'cpu'
     switch (userAttack) {
       case 'rock':
         if (cpuAttack == 'paper') {
